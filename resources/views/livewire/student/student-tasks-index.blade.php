06290e06cf76723374c8ef37155630a7e958a19d
oxytoxin <oxytoxinsgrace@gmail.com>
2021-10-10 14:20:03 +0800

    hotfix on nullsafety

--- resources/views/livewire/student/student-tasks-index.blade.php
+++ resources/views/livewire/student/student-tasks-index.blade.php
@@ -9,41 +9,52 @@
 <div class="p-4">
     <div>
         @if ($current_task)
-            <div x-data="{preview: false}" x-show="preview" x-cloak @open-task-preview.window="preview = true" class="bg-gray-600 bg-opacity-75 inset-0 fixed flex flex-col justify-center items-center p-4 z-10">
-                <div @click.away="preview = false" class="bg-white md:w-1/2 p-4 overflow-y-auto flex-1">
+            <div x-data="{preview: false}" x-show="preview" x-cloak @open-task-preview.window="preview = true"
+                class="fixed inset-0 z-10 flex flex-col items-center justify-center p-4 bg-gray-600 bg-opacity-75">
+                <div @click.away="preview = false" class="flex-1 p-4 overflow-y-auto bg-white md:w-1/2">
                     <h3 class="title-sm">Task Name: {{ $current_task->name }}</h3>
                     <h3 class="title-sm">Instructions: </h3>
-                    <p class="tracking-wider italic px-4 text-sm">{{ $current_task->instructions }}</p>
+                    <p class="px-4 text-sm italic tracking-wider">{{ $current_task->instructions }}</p>
                     <h3 class="title-sm">Task Deadline: {{ $current_task->readable_deadline }}</h3>
-                    <h3 class="title-sm">Highest Score: {{ $current_task->max_score }}/{{ $current_task->max_score }}  pts.</h3>
+                    <h3 class="title-sm">Highest Score:
+                        {{ $current_task->max_score }}/{{ $current_task->max_score }} pts.</h3>
                     @forelse($current_task->questions as $qk => $question)
-                        <div class="@error("questions.$qk.*"){{ 'bg-red-200' }}@enderror mt-4 outline-primary p-4">
-                            <div class="flex md:flex-row flex-col space-y-2 md:space-y-0 items-center mb-2 justify-between">
-                                <h3 class="title">Question {{ $loop->iteration }}. ({{ strtoupper($question['type']) }})</h3>
-                                <h3 class="title-sm bg-green-400 text-gray-900 py-1 px-2 rounded-full font-semibold">{{ $question['points'] }} pts.</h3>
+                        <div class="@error(" questions.$qk.*"){{ 'bg-red-200' }}@enderror mt-4 outline-primary
+                            p-4">
+                            <div
+                                class="flex flex-col items-center justify-between mb-2 space-y-2 md:flex-row md:space-y-0">
+                                <h3 class="title">Question {{ $loop->iteration }}.
+                                    ({{ strtoupper($question['type']) }})</h3>
+                                <h3 class="px-2 py-1 font-semibold text-gray-900 bg-green-400 rounded-full title-sm">
+                                    {{ $question['points'] }} pts.</h3>
                             </div>
-                            <p class="whitespace-pre-wrap px-4 tracking-wider italic text-sm">{{ $question['body'] }}</p>
+                            <p class="px-4 text-sm italic tracking-wider whitespace-pre-wrap">{{ $question['body'] }}
+                            </p>
                             @if ($submission)
                                 <div class="my-2">
                                     <h3 class="title-sm">Your Answers</h3>
                                     <ul class="px-4">
                                         @if (is_array(collect($submission->answers)->firstWhere('identifier', $question['identifier'])['answers']))
                                             @forelse (collect($submission->answers)->firstWhere('identifier', $question['identifier'])['answers'] as $ak => $answer)
-                                                <li class="font-medium text-sm">
+                                                <li class="text-sm font-medium">
                                                     <span>&gt; {{ $answer }}</span>
                                                 </li>
                                             @empty
                                                 @if ($submission?->getMedia()->where('custom_properties.identifier', $question['identifier'])->count())
-                                                    <li class="font-medium text-sm">File attached.</li>
+                                                    <li class="text-sm font-medium">File attached.</li>
                                                 @else
-                                                    <li class="font-medium text-sm">No answers provided.</li>
+                                                    <li class="text-sm font-medium">No answers provided.</li>
                                                 @endif
                                             @endforelse
                                         @else
                                             @if ($question['type'] === 'essay')
-                                                <p class="italic tracking-wider whitespace-pre-wrap px-4 text-sm">{{ collect($submission->answers)->firstWhere('identifier', $question['identifier'])['answers'] }}</p>
+                                                <p class="px-4 text-sm italic tracking-wider whitespace-pre-wrap">
+                                                    {{ collect($submission->answers)->firstWhere('identifier', $question['identifier'])['answers'] }}
+                                                </p>
                                             @else
-                                                <li class="font-medium text-sm">&gt; {{ collect($submission->answers)->firstWhere('identifier', $question['identifier'])['answers'] }}</li>
+                                                <li class="text-sm font-medium">&gt;
+                                                    {{ collect($submission->answers)->firstWhere('identifier', $question['identifier'])['answers'] }}
+                                                </li>
                                             @endif
                                         @endif
 
@@ -56,12 +67,13 @@
                                     <ul class="px-4">
                                         @forelse ($submission->getMedia()->where('custom_properties.identifier', $question['identifier']) as $fak => $file_attachment)
                                             <a target="_blank" href="{{ $file_attachment->getUrl() }}">
-                                                <li class="font-medium underline text-sm">
-                                                    <span class="italic text-green-700">{{ $file_attachment->name }}</span>
+                                                <li class="text-sm font-medium underline">
+                                                    <span
+                                                        class="italic text-green-700">{{ $file_attachment->name }}</span>
                                                 </li>
                                             </a>
                                         @empty
-                                            <li class="font-medium text-sm">No attachments provided.</li>
+                                            <li class="text-sm font-medium">No attachments provided.</li>
                                         @endforelse
                                     </ul>
                                 </div>
@@ -73,7 +85,7 @@
                                         @forelse ($question['choices'] as $ck => $choice)
                                             <h4 class="title-sm">&gt; {{ $choice }}</h4>
                                         @empty
-                                            <li class="font-medium text-sm">No choices provided.</li>
+                                            <li class="text-sm font-medium">No choices provided.</li>
                                         @endforelse
                                     </ul>
                                 </div>
@@ -84,12 +96,13 @@
                                     <ul class="px-4">
                                         @forelse ($attachments->where('custom_properties.identifier', $question['identifier']) as $atk => $attachment)
                                             <a target="_blank" href="{{ $attachment->getUrl() }}">
-                                                <li class="font-medium underline text-sm">
-                                                    <span class="italic text-green-700">{{ $attachment->name }}</span>
+                                                <li class="text-sm font-medium underline">
+                                                    <span
+                                                        class="italic text-green-700">{{ $attachment->name }}</span>
                                                 </li>
                                             </a>
                                         @empty
-                                            <li class="font-medium text-sm">No files attached.</li>
+                                            <li class="text-sm font-medium">No files attached.</li>
                                         @endforelse
                                     </ul>
                                 </div>
@@ -98,62 +111,71 @@
                                 <h1 class="title-sm">File is required.</h1>
                             @endif
                         </div>
-                    @empty
-                        <div class="mt-4 outline-primary p-4">
-                            <h3 class="title text-center">No questions created.</h3>
-                        </div>
-                    @endforelse
+                        @empty
+                            <div class="p-4 mt-4 outline-primary">
+                                <h3 class="text-center title">No questions created.</h3>
+                            </div>
+                        @endforelse
+                    </div>
                 </div>
-            </div>
-        @endif
-        <ul class="grid-list">
-            @forelse ($tasks as $task)
-                <li class="col-span-1 border bg-white rounded-lg shadow divide-y divide-gray-200">
-                    <div class="w-full flex items-center justify-between p-6 space-x-6">
-                        <div class="flex-1 truncate">
-                            <div class="flex items-center space-x-3">
-                                <h3 class="text-gray-900 text-sm font-medium truncate">{{ $task->name }}</h3>
-                                <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ $task->getMedia()->count() }} attachments</span>
+            @endif
+            <ul class="grid-list">
+                @forelse ($tasks as $task)
+                    <li class="col-span-1 bg-white border divide-y divide-gray-200 rounded-lg shadow">
+                        <div class="flex items-center justify-between w-full p-6 space-x-6">
+                            <div class="flex-1 truncate">
+                                <div class="flex items-center space-x-3">
+                                    <h3 class="text-sm font-medium text-gray-900 truncate">{{ $task->name }}</h3>
+                                    <span
+                                        class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ $task->getMedia()->count() }}
+                                        attachments</span>
+                                </div>
+                                <p class="mt-1 text-xs text-gray-500 truncate">Date Created:
+                                    {{ $task->readable_date_created }}</p>
+                                <p class="mt-1 text-xs text-gray-500 truncate">Deadline: {{ $task->readable_deadline }}
+                                </p>
                             </div>
-                            <p class="mt-1 text-gray-500 text-xs truncate">Date Created: {{ $task->readable_date_created }}</p>
-                            <p class="mt-1 text-gray-500 text-xs truncate">Deadline: {{ $task->readable_deadline }}</p>
+                            <img class="flex-shrink-0 w-10 h-10 bg-gray-300 rounded-full"
+                                src="{{ uiavatar($task->name) }}" alt="">
                         </div>
-                        <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src="{{ uiavatar($task->name) }}" alt="">
-                    </div>
-                    <div>
-                        <div class="-mt-px flex divide-x divide-gray-200">
-                            <div class="w-0 flex-1 flex">
-                                @if ($task->user_submission)
-                                    <a href="#" class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
-                                        <!-- Heroicon name: solid/phone -->
-                                        @if ($submission->date_graded)
-                                            <span class="ml-3 underline text-sm">Score: {{$submission->score}}/{{$task->max_score}} pts</span>
-                                        @else
-                                            <x-gmdi-check-r class="text-gray-400"/>
-                                            <span class="ml-3">Submitted</span>
-                                        @endif
-                                    </a>
-                                @else
-                                    <a href="{{ route('student.task.answer', ['task' => $task]) }}" class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
-                                        <!-- Heroicon name: solid/phone -->
-                                        <x-gmdi-edit-r class="text-gray-400"/>
-                                        <span class="ml-3">Answer</span>
-                                    </a>
-                                @endif
-                            </div>
-                            <div class="-ml-px w-0 flex-1 flex">
-                                <button wire:click="preview_task({{$task}})" class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
-                                    <!-- Heroicon name: solid/mail -->
-                                    <x-gmdi-view-in-ar-r class="text-gray-400"/>
-                                    <span class="ml-3">Preview</span>
-                                </button>
+                        <div>
+                            <div class="flex -mt-px divide-x divide-gray-200">
+                                <div class="flex flex-1 w-0">
+                                    @if ($task->user_submission)
+                                        <a href="#"
+                                            class="relative inline-flex items-center justify-center flex-1 w-0 py-4 text-sm font-medium text-gray-700 border border-transparent rounded-br-lg hover:text-gray-500">
+                                            <!-- Heroicon name: solid/phone -->
+                                            @if ($submission?->date_graded)
+                                                <span class="ml-3 text-sm underline">Score:
+                                                    {{ $submission->score }}/{{ $task->max_score }} pts</span>
+                                            @else
+                                                <x-gmdi-check-r class="text-gray-400" />
+                                                <span class="ml-3">Submitted</span>
+                                            @endif
+                                        </a>
+                                    @else
+                                        <a href="{{ route('student.task.answer', ['task' => $task]) }}"
+                                            class="relative inline-flex items-center justify-center flex-1 w-0 py-4 text-sm font-medium text-gray-700 border border-transparent rounded-br-lg hover:text-gray-500">
+                                            <!-- Heroicon name: solid/phone -->
+                                            <x-gmdi-edit-r class="text-gray-400" />
+                                            <span class="ml-3">Answer</span>
+                                        </a>
+                                    @endif
+                                </div>
+                                <div class="flex flex-1 w-0 -ml-px">
+                                    <button wire:click="preview_task({{ $task }})"
+                                        class="relative inline-flex items-center justify-center flex-1 w-0 py-4 -mr-px text-sm font-medium text-gray-700 border border-transparent rounded-bl-lg hover:text-gray-500">
+                                        <!-- Heroicon name: solid/mail -->
+                                        <x-gmdi-view-in-ar-r class="text-gray-400" />
+                                        <span class="ml-3">Preview</span>
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-            @empty
-                <h3 class="title col-span-4 my-10 md:text-2xl text-center">No tasks yet for this course.</h3>
-            @endforelse
-        </ul>
+                    </li>
+                @empty
+                    <h3 class="col-span-4 my-10 text-center title md:text-2xl">No tasks yet for this course.</h3>
+                @endforelse
+            </ul>
+        </div>
     </div>
-</div>
