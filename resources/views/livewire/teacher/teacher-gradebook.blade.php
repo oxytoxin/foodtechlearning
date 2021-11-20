7ea5f6409e35ab8419bcdcbe46e8f45ae7414d1c
oxytoxin <oxytoxinsgrace@gmail.com>
2021-11-20 10:38:03 +0800

    gradebook regression

--- resources/views/livewire/teacher/teacher-gradebook.blade.php
+++ resources/views/livewire/teacher/teacher-gradebook.blade.php
@@ -16,8 +16,10 @@
                 @endforelse
             </select>
         </div>
-        <a href="{{ route('teacher.course.roll_call', ['course' => $course_id]) }}"
-           class="flex items-center justify-center px-16 button-danger">Roll Call</a>
+        @if ($course_id)
+            <a href="{{ route('teacher.course.roll_call', ['course' => $course_id]) }}"
+               class="flex items-center justify-center px-16 button-danger">Roll Call</a>
+        @endif
     </div>
 
     <div>
